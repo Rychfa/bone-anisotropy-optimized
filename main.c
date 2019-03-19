636c6bcc4860a91c69c0087d993e5a27a1c629eb
Hector Joao Rivera Verduzco <hector.rivera@inf.ethz.ch>
2019-03-19 18:17:31 +0100

    Skeleton.

---
+++ main.c
@@ -0,0 +1,47 @@
+/**    ____                   __  ___
+*     / __ )____  ____  ___  /  |/  /___ _____
+*    / __  / __ \/ __ \/ _ \/ /|_/ / __ `/ __ \
+*   / /_/ / /_/ / / / /  __/ /  / / /_/ / /_/ /
+*  /_____/\____/_/ /_/\___/_/  /_/\__,_/ .___/
+*                                     /_/
+*  Bone Anisotropy Mapping.
+*
+*  Copyright (C) 2019  Jarunan Panyasantisuk (jarunan@ethz.ch)
+*                      Rajan Gill            (rgill@ethz.ch)
+*                      Ryan Cherifa          (rcherifa@student.ethz.ch)
+*                      Joao Rivera           (hector.rivera@inf.ethz.ch)
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include "stdio.h"
+#include "reader.h"
+#include "coord_map.h"
+#include "region_extraction.h"
+#include "bvtv.h"
+#include "mil.h"
+
+int main () {
+    //
+    // Call everything
+    //
+    reader();
+    coordMap();
+    region_extraction();
+    mil();
+    bvtv();
+
+    printf("Hello world!\n");
+
+    return 0;
+}
---
+++ regions/region_extraction.h
@@ -0,0 +1,35 @@
+/**    ____                   __  ___
+*     / __ )____  ____  ___  /  |/  /___ _____
+*    / __  / __ \/ __ \/ _ \/ /|_/ / __ `/ __ \
+*   / /_/ / /_/ / / / /  __/ /  / / /_/ / /_/ /
+*  /_____/\____/_/ /_/\___/_/  /_/\__,_/ .___/
+*                                     /_/
+*  Bone Anisotropy Mapping.
+*
+*  Copyright (C) 2019  Jarunan Panyasantisuk (jarunan@ethz.ch)
+*                      Rajan Gill            (rgill@ethz.ch)
+*                      Ryan Cherifa          (rcherifa@student.ethz.ch)
+*                      Joao Rivera           (hector.rivera@inf.ethz.ch)
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef BONEMAP_REGION_EXTRACTION_H
+#define BONEMAP_REGION_EXTRACTION_H
+
+///
+/// Function declarations
+///
+void region_extraction ();
+
+#endif //BONEMAP_REGION_EXTRACTION_H
